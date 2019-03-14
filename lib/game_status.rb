896424555aa3a736b879fe730d1432bf12d90b5a
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-14 15:20:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,36 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+  
+def won?(board)
+  WIN_COMBINATIONS.find do |wins|
+    
+end
+
+def full?(board)
+  board.all?{|answer| answer == "X" || answer = "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  full?(board) || won?(board)
+end
+
+def winner(board)
+  if win_combination = won?(board)
+    board[win_combination.first]
+  end
+end
